ac56fcf4b93ad555a6d6bb27869eb5714cfd7127
wankhede04 <wankhedevijay04@gmail.com>
2023-09-27 22:08:30 +0530

    update hardhat config for compile and network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+
+dotEnvConfig();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // same address and port for both Buidler and Ganache node
+      accounts: [/* will be provided by ganache */],
+      gas: 8000000,
+      gasPrice: 1,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
