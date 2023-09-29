d5d49316a4ed4961b3423b7e115c912c419574ad
wankhede04 <wankhedevijay04@gmail.com>
2023-09-29 17:43:24 +0530

    add deploy script for VotingEscrow

--- scripts/VotingEscrow.deploy.ts
+++ scripts/VotingEscrow.deploy.ts
@@ -1,24 +1,44 @@
-import { ethers } from "hardhat";
+import { ethers , run} from "hardhat"
 
 import dotenv from "dotenv";
 import { ADDRESS_ZERO } from "../test/utilities";
 dotenv.config();
 
 async function main() {
-  let lpToken = process.env.LP_TOKEN_ADDRESS;
   let gaugeControllerAddress = process.env.GAUGE_CONTROLLER_ADDRESS;
+
+  const Token = await ethers.getContractFactory("MockToken");
+  const token = await Token.deploy();
 
   const VotingEscrow = await ethers.getContractFactory("VotingEscrow");
   const votingEscrow = await VotingEscrow.deploy(
-    `${lpToken}`,
+    token.address,
     `${gaugeControllerAddress}`,
-    "voting escrow",
-    "vt",
-    "v.0.1"
+    "Vote Escrowed SYNTH",
+    "veSYNTH",
+    "1"
   );
 
   await votingEscrow.deployed();
   console.log(`VotingEscrow deployed to ${votingEscrow.address}`);
+  console.log(`Token deployed to ${token.address}`);
+
+  await run("verify:verify", {
+    address: votingEscrow.address,
+    constructorArguments: [
+      token.address,
+      `${gaugeControllerAddress}`,
+      "Vote Escrowed SYNTH",
+      "veSYNTH",
+      "1"
+    ],
+  });
+
+  await run("verify:verify", {
+    address: token.address,
+    constructorArguments: [
+    ],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
