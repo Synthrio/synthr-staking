5ba0450a8c909d5f149b15fdbeed8f2427bbef88
wankhede04 <wankhedevijay04@gmail.com>
2023-10-05 12:55:44 +0530

    feat: add getTokenIndex function

--- contracts/farming/DerivedDexLpFarming.sol
+++ contracts/farming/DerivedDexLpFarming.sol
@@ -26,6 +26,19 @@ contract DerivedDexLpFarming is Ownable2Step, BaseDexLpFarming {
         nativeToken = _nativeToken;
     }
 
+    function getIndex(
+        uint256 _pid,
+        uint256 _tokenId
+    ) external view returns (uint256) {
+        uint256[] memory tokenIds = userToken[_pid][msg.sender];
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            if (tokenIds[i] == _tokenId) {
+                return i;
+            }
+        }
+        revert("token id not present");
+    }
+
     /// @notice View function to see pending reward on frontend.
     /// @param _pid The index of the pool. See `poolInfo`.
     /// @param _user Address of user.
@@ -71,26 +84,26 @@ contract DerivedDexLpFarming is Ownable2Step, BaseDexLpFarming {
 
     /// @notice Withdraw LP tokens from DexLpFarming.
     /// @param pid The index of the pool. See `poolInfo`.
-    /// @param tokenIds LP token ids to withdraw.
-    function withdrawBatch(uint256 pid, uint256[] memory tokenIds) public {
+    /// @param tokenIdsIndex LP token ids to withdraw.
+    function withdrawBatch(uint256 pid, uint256[] memory tokenIdsIndex) public {
         PoolInfo memory pool = updatePool(pid);
         UserInfo memory user = userInfo[pid][msg.sender];
         require(user.amount != 0, "DexLpFarming: can not withdraw");
 
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            _withdraw(pid, tokenIds[i], user, pool.accRewardPerShare);
+        for (uint256 i = 0; i < tokenIdsIndex.length; i++) {
+            _withdraw(pid, tokenIdsIndex[i], user, pool.accRewardPerShare);
         }
     }
 
     /// @notice Withdraw LP tokens from DexLpFarming.
     /// @param pid The index of the pool. See `poolInfo`.
-    /// @param tokenId LP token id to withdraw.
-    function withdraw(uint256 pid, uint256 tokenId) public {
+    /// @param tokenIdIndex LP token id index to withdraw.
+    function withdraw(uint256 pid, uint256 tokenIdIndex) public {
         PoolInfo memory pool = updatePool(pid);
         UserInfo memory user = userInfo[pid][msg.sender];
         require(user.amount != 0, "DexLpFarming: can not withdraw");
 
-        _withdraw(pid, tokenId, user, pool.accRewardPerShare);
+        _withdraw(pid, tokenIdIndex, user, pool.accRewardPerShare);
     }
 
     /// @notice Harvest proceeds for transaction sender to `to`.
@@ -98,30 +111,27 @@ contract DerivedDexLpFarming is Ownable2Step, BaseDexLpFarming {
     /// @param to Receiver of REWARD_TOKEN rewards.
     function harvest(uint256 pid, address to) external {
         PoolInfo memory pool = updatePool(pid);
-        _harvest(pid, pool, to);
+        _harvest(pid, pool.accRewardPerShare, to);
     }
 
     /// @notice Withdraw LP tokens from DexLpFarming and harvest proceeds for transaction sender to `to`.
     /// @param pid The index of the pool. See `poolInfo`.
-    /// @param tokenId LP token id to withdraw.
+    /// @param tokenIdIndex LP token id index to withdraw.
     function withdrawAndHarvest(
         uint256 pid,
-        uint256 tokenId,
+        uint256 tokenIdIndex,
         address to
     ) public {
         PoolInfo memory pool = updatePool(pid);
         UserInfo memory _user = userInfo[pid][msg.sender];
         require(_user.amount != 0, "Farming: can not withdraw");
         uint256[] memory _userTokenIds = userToken[pid][msg.sender];
-        for (uint256 i = 0; i < _userTokenIds.length; i++) {
-            if (_userTokenIds[i] == tokenId) {
-                _userTokenIds[i] = _userTokenIds[_userTokenIds.length - 1];
-                break;
-            }
-        }
+
+        uint256 _tokenId = _userTokenIds[tokenIdIndex];
+        _userTokenIds[tokenIdIndex] = _userTokenIds[_userTokenIds.length - 1];
 
         (, , , , , , , uint256 liquidity, , , , ) = tokenTracker.positions(
-            tokenId
+            _tokenId
         );
         uint256 userAmount = _user.amount;
         _user.amount -= liquidity;
@@ -133,10 +143,18 @@ contract DerivedDexLpFarming is Ownable2Step, BaseDexLpFarming {
         }
 
         // Effects
-        _withdrawAndHarvest(pid, pool, tokenId, 1, to, userAmount, _user);
+        _withdrawAndHarvest(
+            pid,
+            pool.accRewardPerShare,
+            _tokenId,
+            liquidity,
+            to,
+            userAmount,
+            _user
+        );
 
         // Interactions
-        tokenTracker.transferFrom(address(this), to, tokenId);
+        tokenTracker.transferFrom(address(this), to, _tokenId);
     }
 
     /// @notice Withdraw without caring about rewards. EMERGENCY ONLY.
@@ -171,17 +189,21 @@ contract DerivedDexLpFarming is Ownable2Step, BaseDexLpFarming {
 
     function _withdraw(
         uint _pid,
-        uint256 _tokenId,
+        uint256 _tokenIdIndex,
         UserInfo memory _user,
         uint256 _accRewardPerShare
     ) internal {
         uint256[] memory _userTokenIds = userToken[_pid][msg.sender];
-        for (uint256 i = 0; i < _userTokenIds.length; i++) {
-            if (_userTokenIds[i] == _tokenId) {
-                _userTokenIds[i] = _userTokenIds[_userTokenIds.length - 1];
-                break;
-            }
-        }
+
+        uint256 _tokenId = _userTokenIds[_tokenIdIndex];
+
+        _userTokenIds[_tokenIdIndex] = _userTokenIds[_userTokenIds.length - 1];
+
+        (, , , , , , , uint256 liquidity, , , , ) = tokenTracker.positions(
+            _tokenId
+        );
+
+        _user.amount -= liquidity;
 
         if (_user.amount != 0) {
             userToken[_pid][msg.sender] = _userTokenIds;
@@ -189,9 +211,6 @@ contract DerivedDexLpFarming is Ownable2Step, BaseDexLpFarming {
         } else {
             delete userToken[_pid][msg.sender];
         }
-        (, , , , , , , uint256 liquidity, , , , ) = tokenTracker.positions(
-            _tokenId
-        );
 
         _withdrawLiquidity(
             _pid,
