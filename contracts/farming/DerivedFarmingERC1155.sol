0ac8805d6f52af7199cc0d0027b05f20af6be7a4
wankhede04 <wankhedevijay04@gmail.com>
2023-10-05 12:57:23 +0530

    feat: add getTokenIndex function and remove deacreaseAmount internal function

--- contracts/farming/DerivedFarmingERC1155.sol
+++ contracts/farming/DerivedFarmingERC1155.sol
@@ -22,6 +22,19 @@ contract DerivedDexLpFarmingERC1155 is Ownable2Step, BaseDexLpFarming {
         LBPair = _LBPair;
     }
 
+    function getIndex(
+        uint256 _pid,
+        uint256 _tokenId
+    ) external view returns (uint256) {
+        uint256[] memory tokenIds = userToken[_pid][msg.sender];
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            if (tokenIds[i] == _tokenId) {
+                return i;
+            }
+        }
+        revert("token id not present");
+    }
+
     /// @notice View function to see pending reward on frontend.
     /// @param _pid The index of the pool. See `poolInfo`.
     /// @param _user Address of user.
@@ -74,31 +87,55 @@ contract DerivedDexLpFarmingERC1155 is Ownable2Step, BaseDexLpFarming {
 
     /// @notice Withdraw LP tokens from DexLpFarming.
     /// @param pid The index of the pool. See `poolInfo`.
-    /// @param tokenIds LP token ids to withdraw.
-    function withdrawBatch(
-        uint256 pid,
-        uint256[] memory tokenIds,
-        uint256[] memory tokenAmounts
-    ) public {
+    /// @param tokenIdsIndex LP token ids indexes to withdraw.
+    function withdrawBatch(uint256 pid, uint256[] memory tokenIdsIndex) public {
         PoolInfo memory pool = updatePool(pid);
         UserInfo memory user = userInfo[pid][msg.sender];
         require(user.amount != 0, "DexLpFarming: can not withdraw");
 
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            _withdraw(
+        uint256[] memory ids = new uint256[](tokenIdsIndex.length);
+        uint256[] memory tokenAmount = new uint256[](tokenIdsIndex.length);
+
+        uint256[] memory _userTokenIds = userToken[pid][msg.sender];
+        uint256[] memory _tokenAmounts = amountOfId[pid][msg.sender];
+
+        for (uint256 i = 0; i < tokenIdsIndex.length; i++) {
+            uint256 _tokenId = _userTokenIds[tokenIdsIndex[i]];
+            (, uint256 _liquidity) = LBPair.getBin(uint24(_tokenId));
+
+            ids[i] = _userTokenIds[tokenIdsIndex[i]];
+            tokenAmount[i] = _tokenAmounts[tokenIdsIndex[i]];
+
+            _tokenAmounts[tokenIdsIndex[i]] = _tokenAmounts[
+                _tokenAmounts.length - 1
+            ];
+            _userTokenIds[tokenIdsIndex[i]] = _userTokenIds[
+                _userTokenIds.length - 1
+            ];
+
+            user.amount -= _liquidity;
+            if (user.amount != 0) {
+                amountOfId[pid][msg.sender] = _tokenAmounts;
+                amountOfId[pid][msg.sender].pop();
+                userToken[pid][msg.sender] = _userTokenIds;
+                userToken[pid][msg.sender].pop();
+            } else {
+                delete userToken[pid][msg.sender];
+                delete amountOfId[pid][msg.sender];
+            }
+
+            _withdrawLiquidity(
                 pid,
-                tokenIds[i],
-                tokenAmounts[i],
+                _tokenId,
                 user,
-                pool.accRewardPerShare
+                pool.accRewardPerShare,
+                _liquidity
             );
+
+            emit Withdraw(msg.sender, pid, _tokenId);
         }
-        LBPair.batchTransferFrom(
-            address(this),
-            msg.sender,
-            tokenIds,
-            tokenAmounts
-        );
+
+        LBPair.batchTransferFrom(address(this), msg.sender, ids, tokenAmount);
     }
 
     /// @notice Harvest proceeds for transaction sender to `to`.
@@ -106,47 +143,64 @@ contract DerivedDexLpFarmingERC1155 is Ownable2Step, BaseDexLpFarming {
     /// @param to Receiver of REWARD_TOKEN rewards.
     function harvest(uint256 pid, address to) external {
         PoolInfo memory pool = updatePool(pid);
-        _harvest(pid, pool, to);
+        _harvest(pid, pool.accRewardPerShare, to);
     }
 
     /// @notice Withdraw LP tokens from DexLpFarming and harvest proceeds for transaction sender to `to`.
     /// @param pid The index of the pool. See `poolInfo`.
-    /// @param tokenId LP token id to withdraw.
+    /// @param tokenIdsIndex LP token id indexes to withdraw.
     function withdrawAndHarvest(
         uint256 pid,
-        uint256[] memory tokenId,
-        uint256[] memory tokensAmount,
+        uint256[] memory tokenIdsIndex,
         address to
     ) public {
         PoolInfo memory pool = updatePool(pid);
         UserInfo memory _user = userInfo[pid][msg.sender];
+        require(_user.amount != 0, "Farming: can not withdraw");
+
         uint256[] memory _userTokenIds = userToken[pid][msg.sender];
         uint256[] memory _tokenAmounts = amountOfId[pid][msg.sender];
-        require(_user.amount != 0, "Farming: can not withdraw");
-        uint256 useAmount = _user.amount;
-        for (uint256 i = 0; i < tokenId.length; i++) {
-            (, uint256 _liquidity) = LBPair.getBin(uint24(tokenId[i]));
-            _decreaseTokenAmount(
-                pid,
-                _user,
-                _userTokenIds,
-                _tokenAmounts,
-                tokenId[i],
-                tokensAmount[i],
-                _liquidity
-            );
+
+        uint256 userAmount = _user.amount;
+
+        uint256[] memory ids = new uint256[](tokenIdsIndex.length);
+        uint256[] memory tokenAmount = new uint256[](tokenIdsIndex.length);
+
+        for (uint256 i = 0; i < tokenIdsIndex.length; i++) {
+            uint256 _tokenId = _userTokenIds[tokenIdsIndex[i]];
+            (, uint256 _liquidity) = LBPair.getBin(uint24(_tokenId));
+            ids[i] = _userTokenIds[tokenIdsIndex[i]];
+            tokenAmount[i] = _tokenAmounts[tokenIdsIndex[i]];
+
+            _tokenAmounts[tokenIdsIndex[i]] = _tokenAmounts[
+                _tokenAmounts.length - 1
+            ];
+            _userTokenIds[tokenIdsIndex[i]] = _userTokenIds[
+                _userTokenIds.length - 1
+            ];
+
+            _user.amount -= _liquidity;
+            if (_user.amount != 0) {
+                amountOfId[pid][msg.sender] = _tokenAmounts;
+                amountOfId[pid][msg.sender].pop();
+                userToken[pid][msg.sender] = _userTokenIds;
+                userToken[pid][msg.sender].pop();
+            } else {
+                delete userToken[pid][msg.sender];
+                delete amountOfId[pid][msg.sender];
+            }
             _withdrawAndHarvest(
                 pid,
-                pool,
-                tokenId[i],
-                tokensAmount[i],
+                pool.accRewardPerShare,
+                _tokenId,
+                _liquidity,
                 to,
-                useAmount,
+                userAmount,
                 _user
             );
         }
         // Interactions
-        LBPair.batchTransferFrom(address(this), to, tokenId, tokensAmount);
+        LBPair.batchTransferFrom(address(this), to, ids, tokenAmount);
     }
 
     /// @notice Withdraw without caring about rewards. EMERGENCY ONLY.
@@ -182,60 +236,5 @@ contract DerivedDexLpFarmingERC1155 is Ownable2Step, BaseDexLpFarming {
         _depositLiquidity(_pid, _tokenId, _user, _accRewardPerShare, liquidity);
 
         emit Deposit(msg.sender, _pid, _tokenId);
-    }
-
-    function _withdraw(
-        uint _pid,
-        uint256 _tokenId,
-        uint256 _tokenAmount,
-        UserInfo memory _user,
-        uint256 _accRewardPerShare
-    ) internal {
-        uint256[] memory _userTokenIds = userToken[_pid][msg.sender];
-        uint256[] memory _tokenAmounts = amountOfId[_pid][msg.sender];
-        (, uint256 _liquidity) = LBPair.getBin(uint24(_tokenId));
-        _decreaseTokenAmount(
-            _pid,
-            _user,
-            _userTokenIds,
-            _tokenAmounts,
-            _tokenId,
-            _tokenAmount,
-            _liquidity
-        );
-
-        _withdrawLiquidity(
-            _pid,
-            _tokenId,
-            _user,
-            _accRewardPerShare,
-            _user.amount
-        );
-
-        emit Withdraw(msg.sender, _pid, _tokenId);
-    }
-
-    function _decreaseTokenAmount(
-        uint _pid,
-        UserInfo memory _user,
-        uint256[] memory _userTokenIds,
-        uint256[] memory _tokenAmounts,
-        uint256 _tokenId,
-        uint256 _tokenAmount,
-        uint256 _liquidity
-    ) internal {
-        for (uint256 i = 0; i < _userTokenIds.length; i++) {
-            if (_userTokenIds[i] == _tokenId) {
-                _tokenAmounts[i] -= _tokenAmount;
-                break;
-            }
-        }
-        _user.amount -= _liquidity;
-        if (_user.amount != 0) {
-            amountOfId[_pid][msg.sender] = _tokenAmounts;
-        } else {
-            delete userToken[_pid][msg.sender];
-            delete amountOfId[_pid][msg.sender];
-        }
     }
 }
