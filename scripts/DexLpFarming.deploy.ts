1d4cef398f83376b50451f8eb8d78bc094bdbbca
wankhede04 <wankhedevijay04@gmail.com>
2023-10-12 21:13:29 +0530

    test: add cross chain test for DexLpFarming (ERC721)

--- scripts/DexLpFarming.deploy.ts
+++ scripts/DexLpFarming.deploy.ts
@@ -8,27 +8,28 @@ async function main() {
   let tokenTracker = process.env.TOKEN_TRACKER;
   let nativeToken = process.env.NATIVE_TOKEN_ADDRESS;
   let liquidityPool = process.env.LIQUIDITY_POOL_ADDRESS;
+  let rewardToken = process.env.REWARD_TOKEN_POLYGON_ADDRESS;
 
-  const MockToken = await ethers.getContractFactory("MockToken");
-  const mockToken = await MockToken.deploy();
-  await mockToken.deployed();
+  // const MockToken = await ethers.getContractFactory("MockToken");
+  // const mockToken = await MockToken.deploy();
+  // await mockToken.deployed();
 
   const DexLpFarming = await ethers.getContractFactory("DerivedDexLpFarming");
-  const dexLpFarming = await DexLpFarming.deploy(mockToken.address,`${tokenTracker}`,`${liquidityPool}`,`${nativeToken}`);
+  const dexLpFarming = await DexLpFarming.deploy(`${rewardToken}`,`${tokenTracker}`,`${liquidityPool}`,`${nativeToken}`);
 
   await dexLpFarming.deployed();
   console.log(`DexLpFarming deployed to ${dexLpFarming.address}`);
-  console.log(`MockToken deployed to ${mockToken.address}`);
+  // console.log(`MockToken deployed to ${mockToken.address}`);
 
-  await run("verify:verify", {
-    address: mockToken.address,
-    constructorArguments: [],
-  });
+  // await run("verify:verify", {
+  //   address: mockToken.address,
+  //   constructorArguments: [],
+  // });
 
   await run("verify:verify", {
     address: dexLpFarming.address,
     constructorArguments: [
-      mockToken.address,
+      `${rewardToken}`,
       `${tokenTracker}`,
       `${liquidityPool}`,
       `${nativeToken}`
