86ca2d2be504b52c38f75115c78d478a6e70ca9f
wankhede04 <wankhedevijay04@gmail.com>
2023-10-20 12:27:33 +0530

    feat: add interface of vote and escrow

---
+++ contracts/interfaces/IVotingEscrow.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+interface IVotingEscrow {
+    function token() external view returns (address);
+
+    function balanceOf(
+        address _user,
+        uint256 _t
+    ) external view returns (uint256);
+}
