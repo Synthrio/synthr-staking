f41c8e311ce2b30ab3b5489e41e768a155e316e4
wankhede04 <wankhedevijay04@gmail.com>
2024-02-20 23:58:07 +0530

    dev: Update config & deployment script

--- scripts/NFTFactory.deploy.ts
+++ scripts/NFTFactory.deploy.ts
@@ -7,18 +7,20 @@ function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 async function main() {
-
+    let owner: any;
+    [owner] =  await ethers.getSigners();
+    console.log(`Owner address check: ${owner.address}`);
     const NFTFactoryInstance = await ethers.getContractFactory("SynthrNFTFactory");
-    const NFTFactory = await NFTFactoryInstance.deploy();
+    const NFTFactory = await NFTFactoryInstance.deploy(owner.address);
 
     await NFTFactory.deployed();
     console.log(`NFT Factory contract deployed to ${NFTFactory.address}`);
 
-    await sleep(60000); // Sleep for 1 minute
+    await sleep(30000); // Sleep for 30 seconds
 
     await run("verify:verify", {
         address: NFTFactory.address,
-        constructorArguments: [],
+        constructorArguments: [owner.address],
     });
 }
 
