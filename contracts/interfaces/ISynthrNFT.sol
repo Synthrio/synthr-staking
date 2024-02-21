50c7c08b914e72effe8783ae0112648057cd6396
wankhede04 <wankhedevijay04@gmail.com>
2024-02-21 15:49:36 +0530

    dev: Remove NFTStaking.sol & updated NFT contract logic

--- contracts/interfaces/ISynthrNFT.sol
+++ contracts/interfaces/ISynthrNFT.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity = 0.8.24;
+pragma solidity =0.8.24;
 
 interface ISynthrNFT {
     function transferFrom(address from, address to, uint256 tokenId) external;
     function lockAmount(uint256 _tokenId) external view returns (uint256);
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
-    function safeMint(address to) external returns (uint256);
-    function safeMintBatch(address[] calldata _to) external;
+    function safeMint(address to, uint256 lpAmount) external returns (uint256);
+    function safeMintBatch(address[] calldata _to, uint256[] calldata _lpAmount) external;
 }
