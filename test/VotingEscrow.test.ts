25b156e9ad039896ebda1e2e10dc63ea06b82755
wankhede04 <wankhedevijay04@gmail.com>
2024-03-05 19:25:39 +0530

    test: added more test cases for VotingEscrow

--- test/VotingEscrow.test.ts
+++ test/VotingEscrow.test.ts
@@ -4,6 +4,8 @@ import { ethers } from "hardhat";
 import { BigNumber } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 import { mine } from "@nomicfoundation/hardhat-network-helpers";
+import { years } from "@nomicfoundation/hardhat-network-helpers/dist/src/helpers/time/duration";
+import { Address } from '../typechain-types/@openzeppelin/contracts/utils/Address';
 
 interface RewardInfo {
   token: string;
@@ -36,6 +38,7 @@ async function setUp() {
   await lpTtoken.mint(Alice.address, parseUnits("100000", 18));
   await lpTtoken.mint(Bob.address, parseUnits("100000", 18));
   await lpTtoken.mint(Joy.address, parseUnits("100000", 18));
+  await rewardToken.mint(owner.address, parseUnits("100000000000000000000", 18));
 
   const VotingEscrow = await ethers.getContractFactory("VotingEscrow");
   votingEscrow = await VotingEscrow.deploy(
@@ -63,7 +66,13 @@ async function addPoolFunc() {
     votingEscrow.address,
     reward
   );
+  await _updateEpochConfigs();
   return tx;
+}
+
+async function _updateEpochConfigs() {
+  await rewardToken.connect(owner).approve(gaugeController.address, parseUnits("100000000000000000000", 18));
+  let updateEpochTx = await gaugeController.connect(owner).updateEpoch(votingEscrow.address, owner.address, [0], [parseUnits("1000", 18)], [parseUnits("100000000000000000000", 18)]);
 }
 
 async function createLock(_value: BigNumber, _unlockTime: BigNumber, signer: any) {
@@ -193,7 +202,6 @@ describe("VotingEscrow", function () {
       expect(tx).to.emit(votingEscrow, "Deposited").withArgs(Alice.address, 0, calUnlockTime2, 3, increaseUnlockTimeTS)
 
     });
-
     it("Should create lock & withdraw lp tokens", async function () {
       await addPoolFunc();
 
@@ -232,6 +240,135 @@ describe("VotingEscrow", function () {
       expect(balanceBeforeLock).to.equal(balanceAfterWithdraw);
 
     });
+
+    it("Should claim reward after sometime of create lock", async function () {
+      await addPoolFunc();
+
+      await lpTtoken
+        .connect(Alice)
+        .approve(votingEscrow.address, parseUnits("1000", 18));
+      let balanceBeforeLock = await lpTtoken.balanceOf(Alice.address);
+      const blockNum = await ethers.provider.getBlockNumber();
+      const block = await ethers.provider.getBlock(blockNum);
+      const timestamp = block.timestamp;
+      let unlockTime = BigNumber.from(timestamp + 1000000);
+      let _value = parseUnits("1000", 18);
+
+      let createLockTxn = await createLock(_value, unlockTime, Alice)
+      let createLockTS = (await ethers.provider.getBlock(createLockTxn.blockNumber)).timestamp;
+
+
+      let calUnlockTime = BigNumber.from(unlockTime)
+        .div(BigNumber.from(604800))
+        .mul(BigNumber.from(604800));
+      let userLockedInfo = await votingEscrow.locked(Alice.address);
+
+      expect(createLockTxn).to.emit(votingEscrow, "Deposited").withArgs(Alice.address, _value, calUnlockTime, 1, createLockTS)
+
+      expect(userLockedInfo.end).to.equal(calUnlockTime);
+      expect(userLockedInfo.amount).to.equal(_value);
+      expect(
+        await gaugeController.userInfo(votingEscrow.address, Alice.address)
+      ).to.equal(_value);
+      mine(1000000); // mining 1000000 blocks after deposit so that to pass lock time
+
+      let rewardBalanceBeforeClaim = await rewardToken.balanceOf(Alice.address);
+      const blockNumAtClaim = await ethers.provider.getBlockNumber() + 1;
+      let expectedRewardAmount = await gaugeController.pendingRewardAtBlock(votingEscrow.address, Alice.address, blockNumAtClaim);
+      let claimTxn = await gaugeController.connect(Alice).claim(votingEscrow.address, Alice.address);
+      await expect(claimTxn).to.emit(gaugeController, "Claimed").withArgs(Alice.address, votingEscrow.address, expectedRewardAmount)
+      let rewardBalanceAfterClaim = await rewardToken.balanceOf(Alice.address);
+      expect(expectedRewardAmount).to.equal(rewardBalanceAfterClaim);
+
+    });
+
+    it("Should claim zero reward if triggered before create lock", async function () {
+      await addPoolFunc();
+      let rewardBalanceBeforeClaim = await rewardToken.balanceOf(Alice.address);
+      let claimTxn = await gaugeController.connect(Alice).claim(votingEscrow.address, Alice.address);
+      await expect(claimTxn).to.emit(gaugeController, "Claimed").withArgs(Alice.address, votingEscrow.address, 0)
+      let rewardBalanceAfterClaim = await rewardToken.balanceOf(Alice.address);
+      expect(rewardBalanceBeforeClaim).to.equal(rewardBalanceAfterClaim);
+    });
+
+
+    it("Should revert if increase lock time is less than current lock time", async function () {
+      await addPoolFunc();
+
+      await lpTtoken
+        .connect(Alice)
+        .approve(votingEscrow.address, parseUnits("1000", 18));
+
+      const blockNum = await ethers.provider.getBlockNumber();
+      const block = await ethers.provider.getBlock(blockNum);
+      const timestamp = block.timestamp;
+      let unlockTime = BigNumber.from(timestamp + 1000000);
+      let _value = parseUnits("1000", 18);
+
+      let createLockTxn = await createLock(_value, unlockTime, Alice)
+      let createLockTS = (await ethers.provider.getBlock(createLockTxn.blockNumber)).timestamp;
+
+
+
+      let calUnlockTime = BigNumber.from(unlockTime)
+        .div(BigNumber.from(604800))
+        .mul(BigNumber.from(604800));
+      let userLockedInfo = await votingEscrow.locked(Alice.address);
+
+      expect(createLockTxn).to.emit(votingEscrow, "Deposited").withArgs(Alice.address, _value, calUnlockTime, 1, createLockTS)
+
+      expect(userLockedInfo.end).to.equal(calUnlockTime);
+      expect(userLockedInfo.amount).to.equal(_value);
+      expect(
+        await gaugeController.userInfo(votingEscrow.address, Alice.address)
+      ).to.equal(_value);
+
+      let newFaultyUnlockTime = unlockTime.sub(BigNumber.from(1000));
+      let calUnlockTime2 = BigNumber.from(newFaultyUnlockTime)
+        .div(BigNumber.from(604800))
+        .mul(BigNumber.from(604800));
+      await expect(votingEscrow.connect(Alice).increaseUnlockTime(newFaultyUnlockTime)).to.revertedWith("VotingEscrow: Can only increase lock duration");
+
+    });
+
+    it("Should revert if increase lock time is more than MAXTIME limit", async function () {
+      await addPoolFunc();
+
+      await lpTtoken
+        .connect(Alice)
+        .approve(votingEscrow.address, parseUnits("1000", 18));
+
+      const blockNum = await ethers.provider.getBlockNumber();
+      const block = await ethers.provider.getBlock(blockNum);
+      const timestamp = block.timestamp;
+      let unlockTime = BigNumber.from(timestamp + 1000000);
+      let _value = parseUnits("1000", 18);
+
+      let createLockTxn = await createLock(_value, unlockTime, Alice)
+      let createLockTS = (await ethers.provider.getBlock(createLockTxn.blockNumber)).timestamp;
+
+
+      let calUnlockTime = BigNumber.from(unlockTime)
+        .div(BigNumber.from(604800))
+        .mul(BigNumber.from(604800));
+      let userLockedInfo = await votingEscrow.locked(Alice.address);
+
+      expect(createLockTxn).to.emit(votingEscrow, "Deposited").withArgs(Alice.address, _value, calUnlockTime, 1, createLockTS)
+
+      expect(userLockedInfo.end).to.equal(calUnlockTime);
+      expect(userLockedInfo.amount).to.equal(_value);
+      expect(
+        await gaugeController.userInfo(votingEscrow.address, Alice.address)
+      ).to.equal(_value);
+
+      const blockNum1 = await ethers.provider.getBlockNumber();
+      const blockTS = (await ethers.provider.getBlock(blockNum1)).timestamp;
+      const MAXTIME = 5 * 365 * 24 * 60 * 60; // 5 years->(negative) || 4 yrs->(positive)
+      let newFaultyUnlockTime = blockTS + MAXTIME;
+      await expect(votingEscrow.connect(Alice).increaseUnlockTime(newFaultyUnlockTime)).to.revertedWith("VotingEscrow: Voting lock can be 4 years max");
+
+    });
+
 
     it("Should revert if create lock & increase lock time with different account", async function () {
       await addPoolFunc();
