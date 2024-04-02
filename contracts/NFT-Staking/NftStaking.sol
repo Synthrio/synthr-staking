9965330cf2bbe83a65f1d8dc155bd58c7c8de0c5
wankhede04 <wankhedevijay04@gmail.com>
2024-04-02 16:32:20 +0530

    fix: underflow in claim and withdrawClaim function

--- contracts/NFT-Staking/NftStaking.sol
+++ contracts/NFT-Staking/NftStaking.sol
@@ -198,7 +198,9 @@ contract NftStaking is IERC721Receiver, Ownable2Step {
         int256 accumulatedReward = _calAccRewardPerShare(_poolInfo.accRewardPerShare, _user.amount);
         uint256 _pendingReward = uint256(accumulatedReward - _user.rewardDebt);
         uint256 _exccessReward = _calculateExcessRewardAtBlock(_user.lockEnd, _user.amount, _poolInfo.rewardPerBlock, block.number);
-        _pendingReward -= _exccessReward;
+        if (_pendingReward < _exccessReward) {
+            _pendingReward -= _exccessReward;
+        }
         // Effects
         _user.rewardDebt = accumulatedReward;
         userInfo[_pool][msg.sender] = _user;
@@ -223,7 +225,10 @@ contract NftStaking is IERC721Receiver, Ownable2Step {
         int256 accumulatedReward = _calAccRewardPerShare(_poolInfo.accRewardPerShare, _user.amount);
         uint256 _pendingReward = uint256(accumulatedReward - (_user.rewardDebt));
         uint256 _exccessReward = _calculateExcessRewardAtBlock(_user.lockEnd, _user.amount, _poolInfo.rewardPerBlock, block.number);
-        _pendingReward -= _exccessReward;
+
+        if (_pendingReward < _exccessReward) {
+            _pendingReward -= _exccessReward;
+        }
 
         // Effects
         _user.rewardDebt = accumulatedReward - (_calAccRewardPerShare(_poolInfo.accRewardPerShare, _lockAmount));
