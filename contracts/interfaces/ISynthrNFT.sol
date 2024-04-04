260deec7ae16a01cae42eb0b2511bf851df05159
wankhede04 <wankhedevijay04@gmail.com>
2024-04-04 20:29:50 +0530

    feat: remove getUserData from synthNFT interface

--- contracts/interfaces/ISynthrNFT.sol
+++ contracts/interfaces/ISynthrNFT.sol
@@ -4,7 +4,6 @@ pragma solidity =0.8.24;
 interface ISynthrNFT {
 
     function transferFrom(address from, address to, uint256 tokenId) external;
-    function getuserData(uint256 _tokenId) external view returns (uint256, uint256);
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
     function safeMint(address to, uint256 lpAmount) external returns (uint256);
     function safeMintBatch(address[] calldata _to, uint256[] calldata _lpAmount) external;
