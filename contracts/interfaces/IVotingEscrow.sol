f7b96d21786d0a5ea14d023eec0429f6f47f60df
wankhede04 <wankhedevijay04@gmail.com>
2024-04-04 18:27:16 +0530

    feat: add locked balance struct in voting escrow interface

--- contracts/interfaces/IVotingEscrow.sol
+++ contracts/interfaces/IVotingEscrow.sol
@@ -2,9 +2,15 @@
 pragma solidity =0.8.24;
 
 interface IVotingEscrow {
+    struct LockedBalance {
+        int128 amount;
+        uint256 end;
+    }
+    
+    function balanceOf(address addr) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function locked(address addr) external view returns (LockedBalance memory);
     function token() external view returns (address);
-
     function balanceOf(address _user, uint256 _t) external view returns (uint256);
-
     function lockedEnd(address _user) external view returns (uint256);
 }
