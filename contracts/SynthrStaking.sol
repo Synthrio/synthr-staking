9c9d60ff69bd0ea888bebd5b515023f44e92fd00
wankhede04 <wankhedevijay04@gmail.com>
2024-04-10 17:23:31 +0530

    feat: removed redundant imports

--- contracts/SynthrStaking.sol
+++ contracts/SynthrStaking.sol
@@ -3,17 +3,15 @@ pragma solidity =0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract SynthrStaking is IERC721Receiver, Ownable, Pausable {
+contract SynthrStaking is Ownable, Pausable {
     using SafeERC20 for IERC20;
 
     /// @notice Address of reward token contract.
     IERC20 public immutable REWARD_TOKEN;
 
     uint256 public constant ACC_REWARD_PRECISION = 1e18;
-    uint256 public constant THREE_MONTHS = 7776000;
 
     /// @notice Info of each gauge controller user.
     /// `amount` LP token amount the user has provided.
