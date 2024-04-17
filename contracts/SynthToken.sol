e2ca173b4f48775ca1ff2d011bdf5c47f0c27887
wankhede04 <wankhedevijay04@gmail.com>
2024-04-17 12:29:33 +0530

    feat: add burnable and pausable functionality in SynthToken

--- contracts/SynthToken.sol
+++ contracts/SynthToken.sol
@@ -3,14 +3,28 @@ pragma solidity =0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract SynthToken is ERC20, Ownable2Step {
+contract SynthToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable2Step {
     constructor(address _owner, address _reciever, uint256 _totalSupply) ERC20("SynthrToken ", "SYNTH") Ownable(_owner) {
         require(_totalSupply > 0, "Synthr: amount must be non zero");
         _mint(_reciever, _totalSupply);
     }
 
-    function burn(address _to, uint256 _amount) public onlyOwner {
-        _burn(_to, _amount);
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following function are overrides required by Solidity.
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
     }
 }
