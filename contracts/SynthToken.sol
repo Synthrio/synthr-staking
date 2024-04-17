2c4180aff265ec846055532a2b371ff95468cb26
wankhede04 <wankhedevijay04@gmail.com>
2024-04-17 13:26:17 +0530

    fix: reciever to recipient

--- contracts/SynthToken.sol
+++ contracts/SynthToken.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.24;
 
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
-import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract SynthToken is ERC20Burnable, Pausable, Ownable2Step {
-    constructor(address _owner, address _reciever, uint256 _totalSupply) ERC20("SYNTHR", "SYNTH") Ownable(_owner) {
+contract Synthr is ERC20Burnable, Pausable, Ownable2Step {
+    constructor(address _owner, address _recipient, uint256 _totalSupply) ERC20("SYNTHR", "SYNTH") Ownable(_owner) {
         require(_totalSupply > 0, "Synthr: amount must be non zero");
-        _mint(_reciever, _totalSupply);
+        _mint(_recipient, _totalSupply);
     }
 
     function pause() public onlyOwner {
