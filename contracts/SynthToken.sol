b573ba5d4eeffdbdf8f39d44495dbdd70ccd51d4
wankhede04 <wankhedevijay04@gmail.com>
2024-04-17 13:03:01 +0530

    feat: remove ERC20Puasable and use Puasable contract

--- contracts/SynthToken.sol
+++ contracts/SynthToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.24;
 
-import "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract SynthToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable2Step {
+contract SynthToken is ERC20Burnable, Pausable, Ownable2Step {
     constructor(address _owner, address _reciever, uint256 _totalSupply) ERC20("SYNTHR", "SYNTH") Ownable(_owner) {
         require(_totalSupply > 0, "Synthr: amount must be non zero");
         _mint(_reciever, _totalSupply);
@@ -19,11 +19,7 @@ contract SynthToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable2Step {
         _unpause();
     }
 
-    // The following function are overrides required by Solidity.
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Pausable)
-    {
+    function _update(address from, address to, uint256 value) internal virtual override whenNotPaused {
         super._update(from, to, value);
     }
 }
