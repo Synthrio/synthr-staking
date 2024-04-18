b6290f0ea6302a083aa7c2145d138a113d9413e6
wankhede04 <wankhedevijay04@gmail.com>
2024-04-18 12:59:59 +0530

    update NFT pools name

--- deploy/01_stWIZARD.ts
+++ deploy/01_stWIZARD.ts
@@ -9,13 +9,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployer } = await getNamedAccounts();
     let owner = deployer;
 
-    await deploy('syMAXI', {
+    await deploy('stWIZARD', {
         from: deployer,
         contract: 'SynthrNFT',
-        args: ['syMAXI', 'syMAXI', owner],
+        args: ['stWIZARD', 'stWIZARD', owner],
         log: true,
         autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     });
 };
 export default func;
-func.tags = ['syMAXI', 'synthrNFTs'];
+func.tags = ['stWIZARD', 'synthrNFTs'];
--- deploy/02_stLEGEND.ts
+++ deploy/02_stLEGEND.ts
@@ -9,13 +9,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployer } = await getNamedAccounts();
     let owner = deployer;
 
-    await deploy('syBULL', {
+    await deploy('stIMMORTAL', {
         from: deployer,
         contract: 'SynthrNFT',
-        args: ['syBULL', 'syBULL', owner],
+        args: ['stIMMORTAL', 'stIMMORTAL', owner],
         log: true,
         autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     });
 };
 export default func;
-func.tags = ['syBULL', 'synthrNFTs'];
+func.tags = ['stLEGEND', 'synthrNFTs'];
